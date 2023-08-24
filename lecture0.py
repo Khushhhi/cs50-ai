086f50bf8998694a70f25bc648c3cb1230ed054c
Khushhhi <108921822+Khushhhi@users.noreply.github.com>
2023-08-24 02:29:16 -0700

    second

--- lecture0.py
+++ lecture0.py
@@ -43,7 +43,7 @@ class Maze():
     def __init__(self, filename) -> None:
         
         #read file and set height and width of the maze
-        with open("filename", "r") as file:
+        with open(filename, "r") as file:
             contents = file.read()
 
         # Defining start and end goal
@@ -51,4 +51,71 @@ class Maze():
             raise Exception("Maze must only have one starting point!")
         if contents.count("B") != 1:
             raise Exception("Maze must only have one ending point!")
+        
+        # determine height and width of maze
+        contents  = contents.splitlines()
+        # print(contents)
+        self.height = len(contents)
+        self.width = max(len(each) for each in contents) 
 
+        #keep track of walls
+        self.walls = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                try:
+                    if contents[i][j] == "A":
+                        self.start = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == "B":
+                        self.goal = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == " ":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+
+        self.solution = None
+        # print(self.walls)
+    
+    def print(self):
+        solution = self.solution[1] if self.solution is not None else None
+        print()
+        for i, row in enumerate(self.walls):
+            for j, col in enumerate(row):
+                if col:
+                    print("█", end="")
+                elif (i, j) == self.start:
+                    print("A", end="")
+                elif (i, j) == self.goal:
+                    print("B", end="")
+                elif solution is not None and (i, j) in solution:
+                    print("*", end="")
+                else:
+                    print(" ", end="")
+            print()
+        print()
+
+    def neighbors(self, state):
+        row, col = state
+        candidates = [
+            ("up", (row-1, col)), 
+            ("down", (row+1, col)), 
+            ("right", (row, col+1)), 
+            ("left", (row, col-1))
+        ]
+        result = []
+
+        for action, (r, c) in candidates:
+            print(action)       
+            if 0 <= r < self.height and 0 <= c < self.width  and not self.walls[r][c]:
+                result.append((action, (r, c)))
+        return result
+
+
+
+m = Maze("maze1.txt")
+m.neighbors((1,2))
